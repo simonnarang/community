4230d151f765885e866e9e3b9c3ada62e8d76fd9
zyan <zbaptis1@jh.edu>
2023-03-17 14:01:26 -0400

    added skeleton code for the event page, added comments as well showing current progress

--- components/Account.tsx
+++ components/Account.tsx
@@ -4,7 +4,7 @@ import Avatar from './Avatar'
 
 import { Database } from '../utils/database.types'
 type Profiles = Database['public']['Tables']['profiles']['Row']
-// type Events = Database['public']['Tables']['events']['Row']
+type Events = Database['public']['Tables']['events']['Row']
 
 
 export default function Account({ session }: { session: Session }) {
@@ -15,6 +15,12 @@ export default function Account({ session }: { session: Session }) {
   const [website, setWebsite] = useState<Profiles['website']>(null)
   const [type, setType] = useState<Profiles['type']>(null)
   const [avatar_url, setAvatarUrl] = useState<Profiles['avatar_url']>(null)
+  const [org_name, setOrgName] = useState<Events['org_name']>(null)
+  const [event_name, setEventName] = useState<Events['event_name']>(null)
+  const [event_flyer, setEventFlyer] = useState<Events['event_flyer']>(null)
+  const [location, setLocation] = useState<Events['location']>(null)
+  const [date, setEventDate] = useState<Events['date']>(null)
+  const [time, setEventTime] = useState<Events['time']>(null)
 
   useEffect(() => {
     async function getProfile() {
@@ -77,24 +83,69 @@ export default function Account({ session }: { session: Session }) {
       if (error) throw error
       alert('Profile updated!')
     } catch (error) {
-      alert('Error updating the data!')
+      alert('Error updating the profile data!')
       console.log(error)
     } finally {
       setLoading(false)
     }
   }
-/*
-code to work on events -- Zyan
+
+useEffect(() => {
+  async function getEvent() {
+    try {
+      setLoading(true)
+      if (!user) throw new Error('No user')
+
+      let { data, error, status } = await supabase
+        .from('events')
+        .select(`username, org_name, event_name, event_flyer, location, date, time, type`)
+        .eq('id', user.id)
+        .single()
+
+      if (error && status !== 406) {
+        throw error
+      }
+
+      if (data) {
+        setUsername(data.username)
+        setType(data.type)
+        setOrgName(data.org_name)
+        setEventName(data.event_name)
+        setEventFlyer(data.event_flyer)
+        setLocation(data.location)
+        setEventDate(data.date)
+        setEventTime(data.time)
+      }
+    } catch (error) {
+      // error here too
+      alert('Error loading event data!')
+      console.log(error)
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  getEvent()
+}, [session, user, supabase])
+
   async function updateEvent({
     username,
-    website,
-    avatar_url,
-    type
+    org_name,
+    event_name,
+    event_flyer,
+    location,
+    date,
+    time,
+    type,
   }: {
-    username: Profiles['username']
-    website: Profiles['website']
-    avatar_url: Profiles['avatar_url']
-    type: Profiles['type']
+    username: Events['username']
+    org_name: Events['org_name']
+    event_name: Events['event_name']
+    event_flyer: Events['event_flyer']
+    location: Events['location']
+    date: Events['date']
+    time: Events['time']
+    type: Events['type']
   }) {
     try {
       setLoading(true)
@@ -102,24 +153,29 @@ code to work on events -- Zyan
 
       const updates = {
         id: user.id,
-        username,
-        website,
-        avatar_url,
+        org_name,
+        event_name,
+        event_flyer,
+        location,
+        date,
+        time,
         updated_at: new Date().toISOString(),
         type,
       }
 
-      let { error } = await supabase.from('profiles').upsert(updates)
+      let { error } = await supabase.from('events').upsert(updates)
       if (error) throw error
-      alert('Profile updated!')
+      alert('Event updated!')
     } catch (error) {
-      alert('Error updating the data!')
+      alert('Error updating the event data!') 
+      // second update button is always throwing this error
+      // need to fix 
       console.log(error)
     } finally {
       setLoading(false)
     }
   }
-*/
+
   return (
     <div className="form-widget">
       <Avatar
@@ -129,6 +185,7 @@ code to work on events -- Zyan
         onUpload={(url) => {
           setAvatarUrl(url)
           updateProfile({ username, website, avatar_url: url, type })
+          updateEvent({ username, org_name, event_name, event_flyer, location, date, time, type })
         }}
       />
       <div>
@@ -164,6 +221,67 @@ code to work on events -- Zyan
         />
       </div>
 
+      {/** need to add these bottom lines to another page as well */}
+      <div>
+        <label htmlFor="org_name">Organization's Name</label>
+        <input
+          id="org_name"
+          type="text"
+          value={org_name || ''}
+          onChange={(e) => setOrgName(e.target.value)}
+        />
+      </div>
+
+      <div>
+        <label htmlFor="event_flyer">Event Flyer</label>
+        <input
+          id="event_flyer"
+          type="text"
+          value={event_flyer || ''}
+          onChange={(e) => setEventFlyer(e.target.value)}
+        />
+      </div>
+
+      <div>
+        <label htmlFor="event_name">Name of Event</label>
+        <input
+          id="event_name"
+          type="text"
+          value={event_name || ''}
+          onChange={(e) => setEventName(e.target.value)}
+        />
+      </div>
+
+      <div>
+        <label htmlFor="location">Location of Event</label>
+        <input
+          id="location"
+          type="text"
+          value={location || ''}
+          onChange={(e) => setLocation(e.target.value)}
+        />
+      </div>
+
+      <div>
+        <label htmlFor="date">Date of Event</label>
+        <input
+          id="date"
+          type="text"
+          value={date || ''}
+          onChange={(e) => setEventDate(e.target.value)}
+        />
+      </div>
+
+      <div>
+        <label htmlFor="time">Time of Event</label>
+        <input
+          id="time"
+          type="text"
+          value={time || ''}
+          onChange={(e) => setEventTime(e.target.value)}
+        />
+      </div>
+
       <div>
         <p></p>
       </div>
@@ -177,18 +295,20 @@ code to work on events -- Zyan
           {loading ? 'Loading ...' : 'Update'}
         </button>
       </div>
-{/* 
-  button to add an event -- Zyan
+
+      {/** this throws an error in line 167 
+       * need to add this to another page as well
+      */} 
       <div>
         <button
           className="button block"
-          onClick={() => updateEvent({ username, eventName, location, date, time, type })}
+          onClick={() => updateEvent({ username, org_name, event_name, event_flyer, location, date, time, type })}
           disabled={loading}
         >
           {loading ? 'Loading ...' : 'Update'}
         </button>
       </div>
-*/}
+
       <div>
         <button className="button block" onClick={() => supabase.auth.signOut()}>
           Sign Out
