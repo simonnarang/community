512a12b724e2959a01013f0331a0f18fbba6f4a0
YisehakE <yisehakebrahim02@gmail.com>
2023-04-03 19:07:00 -0400

    Compartmentalize event types used and refractored Homepage using them

--- pages/Homepage.tsx
+++ pages/Homepage.tsx
@@ -4,26 +4,7 @@ import { useUser, useSupabaseClient, Session } from '@supabase/auth-helpers-reac
 import Link from 'next/link';
 
 import { Database } from '../utils/database.types'
-import { PostgrestError } from '@supabase/supabase-js';
-import { AuthProviders } from '@supabase/auth-ui-react/dist/esm/src/types';
-type Events = Database['public']['Tables']['events']['Row']
-
-
-type EventType = {
-  
-    id: Events["id"],
-    org_name: Events['org_name'],
-    event_name: Events['event_name'],
-    event_flyer: Events['event_flyer'],
-    location: Events['location'],
-    event_time: Events['event_time'],
-    type: Events["type"]
-}
-
-type ApiError = {
-  status: Number,
-  message: string | PostgrestError
-}
+import { Events, EventType } from "../utils/event.types"
 
 const blank_event = {
   id: "", 
@@ -35,23 +16,17 @@ const blank_event = {
   type: ""
 }
 
-
 export default function Homepage({ session }: { session: Session }){
-
-
   const supabase = useSupabaseClient<Database>()
   const user = useUser()
-
 
   const [events, setEvents] = useState<EventType[]>([blank_event]);
 
   const retrieveEvents = async() => {
     try {
-      if (!user) throw new Error('No user' as string)
+      if (!user) throw new Error('No user')
   
-      let { data, error, status } = await supabase
-        .from('events')
-        .select()
+      let { data, error, status } = await supabase.from('events').select()
 
   
       if (error && status !== 406) { throw error}
@@ -76,17 +51,13 @@ export default function Homepage({ session }: { session: Session }){
         setEvents(array)
       }
     } catch (error: any) { // TODO: add more 
-      if (error.message !== "No user") {
-        alert('Error, other than retrieving User, loading event data!')
-      }
-      console.log(error)
+      if (error.message !== "No user") { alert('Error, other than retrieving User, loading event data!') }
     }
   }
 
   useEffect(() => {
     retrieveEvents();
   }, [session, user, supabase]);
-
 
   return (
     <>
@@ -107,14 +78,11 @@ export default function Homepage({ session }: { session: Session }){
       </div>
 
        { JSON.stringify(events) !== "[]" ? events.map((event: Events ) => {  
-         
           return (
-          
             <div key={event.id}>
               {event.event_name}
               <br/>
             </div>
-  
           )
         }) : "No events!"
        }
