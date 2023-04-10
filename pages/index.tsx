efbaab606c7f0f0137d55f60d808223db7ad0094
Simon Narang <snarang5@jhu.edu>
2023-04-10 14:34:22 -0400

    If the user is already logged in, go to the home page with the events feed

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,30 +1,40 @@
 import { Center } from "@mantine/core";
+import { useSession } from "@supabase/auth-helpers-react";
 import Link from "next/link";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default function index() {
-  return (
-    <>
-    <div className="header">
-		<ul>
-			<li>
-				<a href="#">About</a>
-			</li>
-			<li>
-        <Link href="/login">
-        <button id="purple-landing-button">Get Started</button>
-      </Link>
-			</li>
+	const session = useSession()
+	const router = useRouter();
 
-		</ul>
-		<h2>CommUnity</h2>
-		<p>A new  platform that redefines community engagement for college students, on and off campus.</p>
-    <div>
-      <Center>
-        <img src="/landing-image.png" alt="image" id="landing-image" width={300} />
-      </Center>
-    </div>
-	</div>
-  </>
-  )
+	useEffect(() => {
+        if (!session) router.push('/login')
+    })
+	
+	return (
+		<>
+		<div className="header">
+			<ul>
+				<li>
+					<a href="#">About</a>
+				</li>
+				<li>
+			<Link href="/login">
+			<button id="purple-landing-button">Get Started</button>
+		</Link>
+				</li>
+
+			</ul>
+			<h2>CommUnity</h2>
+			<p>A new  platform that redefines community engagement for college students, on and off campus.</p>
+		<div>
+		<Center>
+			<img src="/landing-image.png" alt="image" id="landing-image" width={300} />
+		</Center>
+		</div>
+		</div>
+	</>
+	)
 }
 
