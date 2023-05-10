b2249284adc17dfcd0222b50f77cced8ce1686d5
Simon Narang <snarang5@jhu.edu>
2023-05-09 21:38:04 -0400

    updated landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,28 +13,28 @@ export default function index() {
     })
 	
 	return (
-		<>
-		<div className="header" id="headerLanding">
-			<ul>
-				<li>
-					<a href="https://snarang.notion.site/CommUnity-e44a3d0716384918873b9cb174bbecac">About</a>
-				</li>
-				<li>
-					<Link href="/login">
-					<button id="purple-landing-button">Get Started</button>
-					</Link>
-				</li>
+		<div className="idx">
+			<div className="header" id="headerLanding">
+				<ul>
+					<li>
+						<a href="https://snarang.notion.site/CommUnity-e44a3d0716384918873b9cb174bbecac">About</a>
+					</li>
+					<li>
+						<Link href="/login">
+						<button id="purple-landing-button">Get Started</button>
+						</Link>
+					</li>
 
-			</ul>
-			<h2>CommUnity</h2>
-			<p>A new  platform that redefines community engagement for college students, on and off campus.</p>
-		<div>
-		<Center>
-			<img src="/landing-image.png" alt="image" id="landing-image" width={300} />
-		</Center>
+				</ul>
+				<h2>CommUnity</h2>
+				<p>A new  platform that redefines community engagement for college students, on and off campus.</p>
+			</div>
+			<div className="vid">
+				<video autoPlay muted loop id="myVideo">
+					<source src="video.mp4" type="video/mp4" />
+				</video>
+			</div>
 		</div>
-		</div>
-	</>
 	)
 }
 
